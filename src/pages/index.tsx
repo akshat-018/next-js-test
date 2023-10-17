80bd6fb21e51c095a097d413573199c2ca5e0d24
akshat <akshat@amenses.com>
2023-10-17 17:10:05 +0530

    push for hosting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
-import { render } from "react-dom";
 import Link from "next/link";
 
 export default function Home() {
